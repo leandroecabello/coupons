1df82bf2649c096f153a28e822545654f3b687da
Leandro Cabello <lcabello@gmail.com>
2025-01-16 19:24:28 -0300

    feat(webClient): Update property

--- src/main/java/com/coupon/challenge/config/WebClientConfig.java
+++ src/main/java/com/coupon/challenge/config/WebClientConfig.java
@@ -3,12 +3,16 @@ package com.coupon.challenge.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebClientConfig {
 
+    @Value("${mercadolibre.uri}")
+    private String meliUri;
+
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
-        return builder.baseUrl("https://api.mercadolibre.com").build();
+        return builder.baseUrl(meliUri).build();
     }
 }
